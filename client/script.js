7b9ceb9bc886800c197bd0a59baa9555ef02a26a
Animesh478 <animeshmohanty.am@gmail.com>
2025-10-11 22:03:47 +0530

    structure and styling of ui completed

---
+++ client/script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById("form");
+
+// get all the items from the server
+const getAllItems = function () {};
+
+// display the items
+const displayAllItems = function () {
+  getAllItems();
+};
+
+// when we add a new item
+const addItem = function () {};
+
+const buyItem = function () {}; // when we click the buy button
+
+const init = function () {
+  displayAllItems();
+};
+init();
